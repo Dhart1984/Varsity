673573e2d8fe5491aa62e7602f1a2ecf88edab44
Dhart1984 <donalde.hart@gmail.com>
2023-04-19 16:42:18 -0400

    crud

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from.models import Player
 
 
@@ -10,3 +11,22 @@ def about(request):
 def players_index(request):
     players = Player.objects.all()
     return render(request, 'players/index.html', { 'players' : players})
+def players_detail(request, player_id):
+  player = Player.objects.get(id=player_id)
+  return render(request, 'players/detail.html', { 'player': player})
+
+
+
+
+class PlayerCreate(CreateView):
+    model = Player
+    fields = '__all__'
+
+class PlayerUpdate(UpdateView):
+    model = Player
+    fields = ['name', 'age', 'team',]
+
+class PlayerDelete(DeleteView):
+    model = Player
+    success_url = '/players'
+
