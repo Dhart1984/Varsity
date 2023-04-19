017f7ee502418c465a8509a052eef27bc50aa72f
Dhart1984 <donalde.hart@gmail.com>
2023-04-19 15:09:27 -0400

    index page works first model works

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=75)
+    age = models.IntegerField()
+    sport = models.CharField(max_length=75)
+    league = models.CharField(max_length=75)
+    team = models.CharField(max_length=75)
+    headcoach = models.CharField(max_length=75)
+    asstcoach = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from.models import Player
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+def players_index(request):
+    players = Player.objects.all()
+    return render(request, 'players/index.html', { 'players' : players})
